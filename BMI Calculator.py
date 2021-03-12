aaa98421c611610a08d4d09db602e835a5abfaac
vandyck_lai <76078213+Laikaiyong@users.noreply.github.com>
2021-03-12 15:18:19 +0800

    Update BMI Calculator.py

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -3,11 +3,13 @@ height = float(input("Height in metre (m): "))
 
 bmi = weight / (height ** 2)
 
+print(bmi)
+
 if bmi < 18.5:
-    print(bmi + "\nUnderweight \n"+"Please consume more food and don't starve yourself!")
+    print("\nUnderweight \n"+"Please consume more food and don't starve yourself!")
 elif bmi >= 18.5 and bmi < 25:
-    print(bmi + "\nNormal")
+    print("\nNormal")
 elif bmi >= 25 and bmi < 30:
-    print(bmi + "\nOverweight \n"+"Be aware of your daily diet")
+    print("\nOverweight \n"+"Be aware of your daily diet")
 elif bmi >= 30:
-    print(bmi + "\nObesity \n"+"Gotta exercise to cut down some weight!")
+    print("\nObesity \n"+"Gotta exercise to cut down some weight!")
